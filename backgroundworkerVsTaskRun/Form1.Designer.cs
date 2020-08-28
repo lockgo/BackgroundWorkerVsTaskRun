6ffbd67487c6ba138e4596e8e5d3faa3b709ed73
lockgo <juan.t.chen@gmail.com>
2020-08-27 22:27:22 -0400

    Add files via upload

---
+++ backgroundworkerVsTaskRun/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace backgroundworkerVsTaskRun
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.cancelButton1 = new System.Windows.Forms.Button();
+            this.cancelButton2 = new System.Windows.Forms.Button();
+            this.bgw = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.bothBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 514);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Run1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(571, 514);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Run2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(543, 496);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(571, 12);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(543, 496);
+            this.richTextBox2.TabIndex = 3;
+            this.richTextBox2.Text = "";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(455, 514);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(1014, 514);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(100, 23);
+            this.progressBar2.TabIndex = 5;
+            // 
+            // cancelButton1
+            // 
+            this.cancelButton1.Location = new System.Drawing.Point(374, 514);
+            this.cancelButton1.Name = "cancelButton1";
+            this.cancelButton1.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton1.TabIndex = 6;
+            this.cancelButton1.Text = "Cancel1";
+            this.cancelButton1.UseVisualStyleBackColor = true;
+            this.cancelButton1.Click += new System.EventHandler(this.cancelButton1_Click);
+            // 
+            // cancelButton2
+            // 
+            this.cancelButton2.Location = new System.Drawing.Point(933, 514);
+            this.cancelButton2.Name = "cancelButton2";
+            this.cancelButton2.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton2.TabIndex = 7;
+            this.cancelButton2.Text = "Cancel2";
+            this.cancelButton2.UseVisualStyleBackColor = true;
+            this.cancelButton2.Click += new System.EventHandler(this.cancelButton2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(93, 519);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "label1";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(96, 535);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(87, 17);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "Failure Box 1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(653, 515);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "label2";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(656, 535);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(87, 17);
+            this.checkBox2.TabIndex = 11;
+            this.checkBox2.Text = "Failure Box 2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // bothBtn
+            // 
+            this.bothBtn.Location = new System.Drawing.Point(518, 722);
+            this.bothBtn.Name = "bothBtn";
+            this.bothBtn.Size = new System.Drawing.Size(75, 23);
+            this.bothBtn.TabIndex = 12;
+            this.bothBtn.Text = "both";
+            this.bothBtn.UseVisualStyleBackColor = true;
+            this.bothBtn.Click += new System.EventHandler(this.bothBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1126, 757);
+            this.Controls.Add(this.bothBtn);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cancelButton2);
+            this.Controls.Add(this.cancelButton1);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.Button cancelButton1;
+        private System.Windows.Forms.Button cancelButton2;
+        private System.ComponentModel.BackgroundWorker bgw;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Button bothBtn;
+    }
+}
+
---
+++ backgroundworkerVsTaskRun/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace backgroundworkerVsTaskRun
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Background Worker
+        /// </summary>
+        private BackgroundWorker _bgw;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var fail = checkBox1.Checked;
+            _bgw = new BackgroundWorker();
+            var bgw = _bgw;
+            bgw.WorkerSupportsCancellation = true;
+            bgw.WorkerReportsProgress = true;
+            bgw.DoWork += (_, args) =>
+            {
+                for (int i = 0; i != 100; ++i)
+                {
+                    //bgw.ReportProgress(0, i + "%");
+                    bgw.ReportProgress(i, i + "%");
+                    if (bgw.CancellationPending)
+                    {
+                        args.Cancel = true;
+                        return;
+                    }
+                    Thread.Sleep(100);
+                }
+                if (fail)
+                    throw new InvalidOperationException("Requested to fail.");
+                args.Result = 13;
+            };
+            //bgw.ProgressChanged += (_, args) =>
+            bgw.ProgressChanged += (_, args) =>
+            {
+                label1.Text = (string)args.UserState;
+                richTextBox1.Text = richTextBox1.Text + " " + (string)args.UserState;
+                progressBar1.Value = args.ProgressPercentage;
+            };
+            bgw.RunWorkerCompleted += (_, args) =>
+            {
+                if (args.Cancelled)
+                {
+                    label1.Text = "Cancelled.";
+                }
+                else if (args.Error == null)
+                {
+                    var result = (int)args.Result;
+                    label1.Text = "Completed: " + result;
+                }
+                else
+                {
+                    label1.Text = args.Error.GetType().Name + ": " + args.Error.Message;
+                }
+            };
+            bgw.RunWorkerAsync();
+        }
+        private void cancelButton1_Click(object sender, EventArgs e)    {   if (_bgw != null) _bgw.CancelAsync();   }
+        /// <summary>
+        /// Task.Run
+        /// </summary>
+        private CancellationTokenSource _cts;
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            var fail = checkBox1.Checked;
+            _cts = new CancellationTokenSource();
+            var token = _cts.Token;
+            var progressHandler = new Progress<string>(value =>
+            {
+                label2.Text = value;
+                richTextBox2.Text = richTextBox2.Text + " " + value;
+            });
+            var progress = progressHandler as IProgress<string>;
+            try
+            {
+                var result = await Task.Run(() =>
+                {
+                    for (int i = 0; i != 100; ++i)
+                    {
+                        if (progress != null)
+                            progress.Report(i + "%");
+                        token.ThrowIfCancellationRequested();
+                        Thread.Sleep(100);
+                    }
+                    if (fail)
+                        throw new InvalidOperationException("Requested to fail.");
+                    return 13;
+                });
+                label2.Text = "Completed: " + result;
+            }
+            catch (OperationCanceledException)
+            {                
+                label2.Text = "Cancelled.";            
+            }
+            catch (Exception ex)
+            {                
+                label2.Text = ex.GetType().Name + ": " + ex.Message;            
+            }
+        }
+        private void cancelButton2_Click(object sender, EventArgs e)    {   if (_cts != null) _cts.Cancel();    }
+
+        private void bothBtn_Click(object sender, EventArgs e)
+        {
+            button1_Click(sender, e);
+            button2_Click(sender, e);
+        }
+
+        
+
+    }
+}
